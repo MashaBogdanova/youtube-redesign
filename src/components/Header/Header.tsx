07631586232124b70d8a56bbfdeb719243f6e1b4
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-12-18 14:04:51 +0100

    fix: align youtube logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './header.module.scss';
 import BurgerIcon from '../BurgerIcon/BurgerIcon.tsx';
-// import YouTubeLogo from '../UI/YouTubeLogo/YouTubeLogo.tsx';
+import YouTubeLogo from '../UI/YouTubeLogo/YouTubeLogo.tsx';
 import SearchInput from '../SearchInput/SearchInput.tsx';
 // import HeaderIcons from '../HeaderIcons/HeaderIcons.tsx';
 // import UserPicture from '../UserPicture/UserPicture.tsx';
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <BurgerIcon />
-      {/*<YouTubeLogo />*/}
+      <YouTubeLogo />
       <SearchInput />
       {/*<HeaderIcons />*/}
       {/*<UserPicture />*/}
