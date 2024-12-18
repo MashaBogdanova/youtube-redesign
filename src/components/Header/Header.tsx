ef2d1553eb84455831292631cef3f2153f736fa4
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-12-18 14:14:31 +0100

    fix: align header icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.scss';
 import BurgerIcon from '../BurgerIcon/BurgerIcon.tsx';
 import YouTubeLogo from '../UI/YouTubeLogo/YouTubeLogo.tsx';
 import SearchInput from '../SearchInput/SearchInput.tsx';
-// import HeaderIcons from '../HeaderIcons/HeaderIcons.tsx';
+import HeaderIcons from '../HeaderIcons/HeaderIcons.tsx';
 // import UserPicture from '../UserPicture/UserPicture.tsx';
 
 const Header = () => {
@@ -12,7 +12,7 @@ const Header = () => {
       <BurgerIcon />
       <YouTubeLogo />
       <SearchInput />
-      {/*<HeaderIcons />*/}
+      <HeaderIcons />
       {/*<UserPicture />*/}
     </header>
   );
