767f091e5331e8cae93ce7fa952e8d794f0b13aa
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-12-19 16:24:44 +0100

    fix: pagination right padding

--- src/pages/HomePage/RecommendedChannel/RecommendedChannel.tsx
+++ src/pages/HomePage/RecommendedChannel/RecommendedChannel.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import VideoListSection from '../../../components/UI/VideoListSection/VideoListSection.tsx';
 import { recommendedChannel } from '../../../data/recommended-channel.ts';
 
