b1302c714a469fd461447e003454324d50dd7c46
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-12-19 10:01:44 +0100

    feat: add settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './settings.module.scss';
+import settings from '../../assets/nav-icons/settings.svg';
 
 const Settings = () => {
-  return <div className={styles.settings}>settings</div>;
+  return (
+    <button className={styles.settings} aria-label="Settings">
+      <img src={settings} alt="" />
+      Settings
+    </button>
+  );
 };
 
 export default Settings;
